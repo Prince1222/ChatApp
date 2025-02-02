b50bef0f0b4f28173e97c747f56db06690a1ac34
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-02 14:30:30 +0545

    update of chat

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -3,15 +3,17 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class ChatComponent extends Component
 {
+    public $user;
     public function render()
     {
         return view('livewire.chat-component');
     }
 
     public function mount($user_id){
- 
+    $this->user = User::whereId($user_id)->first();
     }
 }
